e44814609fbe6173d14f46af867281be9e131561
BrienDespenza <yammylove178@gmail.com>
2022-03-24 13:26:10 +0000

    hey hey I have my comments

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -48,6 +48,102 @@ var level01 = function (window) {
         createSawBlade(400, 350);
         createSawBlade(500, 370);
         createSawBlade(250, 350);
+
+
+        function createEnemy(x,y){
+            var enemy = game.createGameItem('enemy',25); //creates game item and stores it in the variable enemy
+        var redSquare = draw.rect(50,50,'red'); //creates rectangle that stores as redSquare
+        redSquare.x = -25; 
+        redSquare.y = -25;
+        enemy.addChild(redSquare); //adds the redSquare to the enemy game item
+
+
+        enemy.x = x;
+        enemy.y = y;
+
+        game.addGameItem(enemy); //adds enemy to the game
+
+        enemy.velocityX = -1; //causes the enemy to move one pixel to the left on the x position
+
+        enemy.rotationalVelocity = 10; //makes enemy rotate
+
+        enemy.onPlayerCollision = function() {
+        console.log('The enemy has hit Halle');
+        game.changeIntegrity(-10) //takes away health when hit
+    };
+
+         enemy.onProjectileCollision = function() {
+        console.log('The projectile has hit Halle');
+        game.changeIntegrity(5)//gives health when player shoots enemy
+        enemy.flyTo(100,100); 
+    };
+        }
+
+        function createReaward (){
+              var reward = game.createGameItem('reward',25); //creates game item and stores it in the variable enemy
+        var blueSquare = draw.rect(50,50,'blue'); //creates rectangle that stores as redSquare
+        blueSquare.x = -25; 
+        blueSquare.y = -25;
+        reward.addChild(blueSquare); //adds the redSquare to the enemy game item
+
+
+        reward.x = x;
+        reward.y = y;
+
+        game.addGameItem(reward); //adds reward to the game
+
+        reward.velocityX = -1; //causes the reward to move one pixel to the left on the x position
+
+        reward.rotationalVelocity = 10; //makes reward rotate
+
+        reward.onPlayerCollision = function() {
+        console.log('The reward has hit Halle');
+        game.changeIntegrity(10) //gives health when hit
+    };
+
+        }
+
+
+        var levelData = {
+        "name": "Robot Romp",
+        "number": 1, 
+        "speed": -3,
+        "gameItems": [
+	{ "type": "sawblade", "x": 400, "y": groundY -50},
+	{ "type": "sawblade", "x": 600, "y": groundY -40},
+	{ "type": "sawblade", "x": 800, "y": groundY -30},
+
+    { "type": "enemy", "x": 400, "y": groundY -50 },
+	{ "type": "enemy", "x": 600, "y": groundY -80},
+	{ "type": "enemy", "x": 900, "y": groundY -30},
+
+    { "type": "reward", "x": 700, "y": groundY -70 },
+	{ "type": "reward", "x": 600, "y": groundY -90},
+	{ "type": "reward", "x": 600, "y": groundY -60},
+    ]
+};
+
+    for(var i = 0; i < levelData.gameItems.length; i++){
+        var gameItem = levelData.gameItems[i];
+
+        if (gameItem.type === "sawblade"){
+            createSawBlade(gameItem.x, gameItem.y);
+        }
+
+        if (gameItem.type === "enemy"){
+            createEnemy(gameItem.x, gameItem.y);
+        }
+
+        if (gameItem.type === "reward"){
+            createReward(gameItem.x, gameItem.y);
+        }
+    }
+
+        
+
+
+
+
         // DO NOT EDIT CODE BELOW HERE
     }
 };
